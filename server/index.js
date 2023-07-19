38dbe4f178a9e6776bd733b34ad6c653de6db8f4
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-20 00:36:03 +0530

    Add create query

--- server/index.js
+++ server/index.js
@@ -5,8 +5,28 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
-app.listen(4000, () => {
-  console.log("server has started on port 4000.");
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
 });
