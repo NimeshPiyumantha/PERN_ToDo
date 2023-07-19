e576b45bacdaa71aa729f46508d30f99ce3ec68b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-20 00:36:55 +0530

    Add Get one on filter all  query

--- server/index.js
+++ server/index.js
@@ -36,6 +36,22 @@ app.get("/todos", async (req, res) => {
   }
 });
 
+//get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
